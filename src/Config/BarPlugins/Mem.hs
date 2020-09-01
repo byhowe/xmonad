44dc02ccc3d7200bfc0b50ee4eefbe963ebcedf5
B. Howe <37745048+byhowe@users.noreply.github.com>
2020-09-01 10:00:11 -0800

    update Mem

--- src/Config/BarPlugins/Mem.hs
+++ src/Config/BarPlugins/Mem.hs
@@ -23,8 +23,10 @@ instance Default Mem where
   def = Mem {alias = "memory", formatter = defaultFormatter, rate = 10}
 
 defaultFormatter :: MemInfo -> String
-defaultFormatter MemInfo {total, available} =
-  let stat = floor $ (fromIntegral $ total - available :: Float) / 1024.0 :: Int
+defaultFormatter MemInfo {total, free, buffers, cache} =
+  let stat =
+        floor $
+        (fromIntegral $ total - free - buffers - cache :: Float) / 1024.0 :: Int
    in show stat ++ "M"
 
 instance Bar.WMExec Mem where
@@ -37,9 +39,10 @@ instance Bar.WMExec Mem where
 
 data MemInfo =
   MemInfo
-    { total     :: Int
-    , free      :: Int
-    , available :: Int
+    { total   :: Int
+    , free    :: Int
+    , buffers :: Int
+    , cache   :: Int
     }
 
 memFile :: String
@@ -47,22 +50,28 @@ memFile = "/proc/meminfo"
 
 getMemInfo :: IO MemInfo
 getMemInfo = do
-  flip catchIOError (const . return $ MemInfo 0 0 0) $ do
+  flip catchIOError (const . return $ MemInfo 0 0 0 0) $ do
     info <- B.readFile memFile
-    return $ getMemInfo' (lines . B.unpack $ info) Nothing Nothing Nothing
+    return $
+      getMemInfo' (lines . B.unpack $ info) Nothing Nothing Nothing Nothing
   where
-    getMemInfo' :: [String] -> Maybe Int -> Maybe Int -> Maybe Int -> MemInfo
-    getMemInfo' _ (Just t) (Just f) (Just a) =
-      MemInfo {total = t, free = f, available = a}
-    getMemInfo' [] t f a =
+    getMemInfo' ::
+         [String] -> Maybe Int -> Maybe Int -> Maybe Int -> Maybe Int -> MemInfo
+    getMemInfo' _ (Just t) (Just f) (Just b) (Just c) =
+      MemInfo {total = t, free = f, buffers = b, cache = c}
+    getMemInfo' [] t f b c =
       MemInfo
-        {total = fromMaybe 0 t, free = fromMaybe 0 f, available = fromMaybe 0 a}
-    getMemInfo' (line:rest) t f a
-      | startsWith line "MemTotal" = getMemInfo' rest (Just $ getInt line) f a
-      | startsWith line "MemFree" = getMemInfo' rest t (Just $ getInt line) a
-      | startsWith line "MemAvailable" =
-        getMemInfo' rest t f (Just $ getInt line)
-      | otherwise = getMemInfo' rest t f a
+        { total = fromMaybe 0 t
+        , free = fromMaybe 0 f
+        , buffers = fromMaybe 0 b
+        , cache = fromMaybe 0 c
+        }
+    getMemInfo' (line:rest) t f b c
+      | startsWith line "MemTotal" = getMemInfo' rest (Just $ getInt line) f b c
+      | startsWith line "MemFree" = getMemInfo' rest t (Just $ getInt line) b c
+      | startsWith line "Buffers" = getMemInfo' rest t f (Just $ getInt line) c
+      | startsWith line "Cached" = getMemInfo' rest t f b (Just $ getInt line)
+      | otherwise = getMemInfo' rest t f b c
     startsWith :: String -> String -> Bool
     startsWith h n = take (length n) h == n
     getInt :: String -> Int
